dd440d269740bc3de0f51bebe49acadc49241015
adamkr13 <adamreuter13@gmail.com>
2021-02-17 17:23:23 -0500

    Modified DaysAvailable in Driver class and added DriverListItem and DriverCreate in Models

---
+++ Boats-4-U.Models/Driver/DriverCreate.cs
@@ -0,0 +1,27 @@
+﻿using Boats_4_U.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boats_4_U.Models.Driver
+{
+    public class DriverCreate
+    {
+        [Required]
+        public string DriverFirstName { get; set; }
+        [Required]
+        public string DriverLastName { get; set; }
+        [Required]
+        public decimal HourlyRate { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public BoatType TypeOfBoat { get; set; }
+        public List<DayOfWeek> DaysAvailable { get; set; } = new List<DayOfWeek>();
+        public int MaximumOccupants { get; set; }
+        public Guid User { get; set; }
+    }
+}
---
+++ Boats-4-U.Models/Driver/DriverListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boats_4_U.Models.Driver
+{
+    public class DriverListItem
+    {
+        public int DriverId { get; set; }
+        public string DriverFullName { get; }                
+    }
+}
