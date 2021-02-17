23be16b970965246b48df0102da9ad0d93b6283c
Caleb Addie <cjaddie.89@gmail.com>
2021-02-16 20:53:18 -0500

    Added identity models and classes as a TEAM

---
+++ Boats-4-U.Data/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boats_4_U.Data
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+
+        [ForeignKey(nameof(Driver))]
+        public int DriverId { get; set; }
+        public virtual Driver Driver { get; set; }
+
+        [ForeignKey(nameof(Renter))]
+        public int RenterId { get; set; }
+        public virtual Renter Renter { get; set; }
+
+        [Required]
+        public decimal ReservationDuration { get; set; }
+
+        [Required]
+        public DateTimeOffset DateReservedFor { get; set; }
+
+        [Required]
+        public DateTimeOffset DateReservationMade { get; set; }
+
+        [Required]
+        public decimal EstimatedTotalCost
+        {
+            get
+            {
+                var estimatedTotalCost = Driver.HourlyRate * ReservationDuration;
+                return estimatedTotalCost;
+            }
+        }
+
+        [Required]
+        public int NumberOfPassengers { get; set; }
+
+        public DateTimeOffset? ModifiedDate { get; set; }
+    }
+}
