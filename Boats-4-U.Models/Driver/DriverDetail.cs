ef18db7640115053a297dbbb860afaeafe230a99
adamkr13 <adamreuter13@gmail.com>
2021-02-18 12:30:58 -0500

    Added DriverDetail model

---
+++ Boats-4-U.Models/Driver/DriverDetail.cs
@@ -0,0 +1,48 @@
+﻿using Boats_4_U.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boats_4_U.Models.Driver
+{
+    class DriverDetail
+    {
+        public int DriverId { get; set; }
+        
+        public string DriverFirstName { get; set; }
+
+        public string DriverLastName { get; set; }
+
+        public decimal HourlyRate { get; set; }
+
+        public string Location { get; set; }
+
+        public BoatType TypeOfBoat { get; set; }
+
+        public List<DayOfWeek> DaysAvailable { get; set; } = new List<DayOfWeek>();
+
+        public int MaximumOccupants { get; set; }
+                
+        public string DriverFullName
+        {
+            get
+            {
+                var fullName = $"{DriverFirstName} {DriverLastName}";
+                return fullName;
+            }
+        }
+        public string BoatName
+        {
+            get
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    string boatName = Enum.GetName(typeof(BoatType), ctx);
+                    return boatName;
+                }
+            }
+        }
+    }
+}
