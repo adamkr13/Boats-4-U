afd5c7a3fc5ad4d00e6c17fdf756cb099f0ca867
Jon Kinne <kinnejs@gmail.com>
2021-02-20 22:29:41 -0500

    Add RenterController

--- Boats-4-U.Models/RenterDetail.cs
+++ Boats-4-U.Models/RenterDetail.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Boats_4_U.Data
+namespace Boats_4_U.Models
 {
-    public class Renter
+    public class RenterDetail
     {
-        [Key]
         public int RenterId { get; set; }
-        [Required]
         public string RenterFirstName { get; set; }
-        [Required]
         public string RenterLastName { get; set; }
-        [Required]
         public int RenterAge { get; set; }
-        [Required]
         public long CreditCardNumber { get; set; }
-        [Required]
-        public Guid User { get; set; }
-
         public string RenterFullName
         {
             get
@@ -30,7 +21,6 @@ namespace Boats_4_U.Data
                 return fullName;
             }
         }
-
         public string Last4Digits
         {
             get
@@ -40,7 +30,5 @@ namespace Boats_4_U.Data
                 return creditCardNumber.Substring(creditCardNumber.Length - 4, 4);
             }
         }
-
-        public virtual List<Reservation> ReservationRenter { get; set; } = new List<Reservation>();
     }
 }
---
+++ Boats-4-U.WebAPI/Controllers/RenterController.cs
@@ -0,0 +1,66 @@
+﻿using Boats_4_U.Models;
+using Boats_4_U.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Boats_4_U.WebAPI.Controllers
+{
+    [Authorize]
+    public class RenterController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            RenterService renterService = CreateRenterService();
+            var renters = renterService.GetRenters();
+            return Ok(renters);
+        }
+        
+        public IHttpActionResult Post(RenterCreate renter)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRenterService();
+
+            if (!service.CreateRenter(renter))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateRenterService();
+
+            if (!service.DeleteRenter(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(RenterUpdate renter)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRenterService();
+
+            if (!service.UpdateRenter(renter))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private RenterService CreateRenterService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var renterService = new RenterService(userId);
+            return renterService;
+        }
+    }
+}
