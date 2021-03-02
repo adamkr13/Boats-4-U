e03c21c95c92bc99f14c0dc8c870a1d67aba7bea
Caleb Addie <cjaddie.89@gmail.com>
2021-03-02 18:26:14 -0500

    Ratings classes created and integrated

--- Boats-4-U.Data/Renter.cs
+++ Boats-4-U.Data/Renter.cs
@@ -58,5 +58,82 @@ namespace Boats_4_U.Data
         }
 
         public virtual List<Reservation> ReservationRenter { get; set; } = new List<Reservation>();
+        public virtual List<RenterRating> RenterRatings { get; set; } = new List<RenterRating>();
+
+        public double Rating
+        {
+            get
+            {
+                double totalAverageRating = 0;
+
+                foreach (var rating in RenterRatings)
+                {
+                    totalAverageRating += rating.AverageRenterRating;
+                }
+
+                return RenterRatings.Count > 0
+                    ? Math.Round(totalAverageRating / RenterRatings.Count, 2)
+                    : 0;
+            }
+        }
+
+        public bool IsRecommended
+        {
+            get
+            {
+                return Rating > 8;
+            }
+        }
+
+        public double FunRating
+        {
+            get
+            {
+                double averageFunRating = 0;
+
+                foreach (RenterRating renterRating in RenterRatings)
+                {
+                    averageFunRating += renterRating.RenterFun;
+                }
+
+                return RenterRatings.Count > 0
+                    ? Math.Round(averageFunRating / RenterRatings.Count, 2)
+                    : 0;
+            }
+        }
+
+        public double SafetyRating
+        {
+            get
+            {
+                double averageSafetyRating = 0;
+
+                foreach (RenterRating renterRating in RenterRatings)
+                {
+                    averageSafetyRating += renterRating.RenterSafety;
+                }
+
+                return RenterRatings.Count > 0
+                    ? Math.Round(averageSafetyRating / RenterRatings.Count, 2)
+                    : 0;
+            }
+        }
+
+        public double CleanlinessRating
+        {
+            get
+            {
+                double averageCleanlinessRating = 0;
+
+                foreach (var renterRating in RenterRatings)
+                {
+                    averageCleanlinessRating += renterRating.RenterCleanliness;
+                }
+
+                return RenterRatings.Count > 0
+                    ? Math.Round(averageCleanlinessRating / RenterRatings.Count, 2)
+                    : 0;
+            }
+        }
     }
 }
