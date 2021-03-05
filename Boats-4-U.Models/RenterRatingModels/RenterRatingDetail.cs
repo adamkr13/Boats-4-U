8350374c37e5497bfc86365e399a5045da101847
Jon Kinne <kinnejs@gmail.com>
2021-03-05 14:44:25 -0500

    RenterRating

---
+++ Boats-4-U.Models/RenterRatingModels/RenterRatingDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boats_4_U.Models
+{
+    public class RenterRatingDetail
+    {
+        public int RenterRatingId { get; set; }
+
+        public Guid ApplicationUser { get; set; }
+
+        public int RenterId { get; set; }
+
+        public string RenterFullName { get; set; }
+
+        public double RenterCleanlinessScore { get; set; }
+
+        public double RenterSafetyScore { get; set; }
+
+        public double RenterPunctualityScore { get; set; }
+
+        public double AverageRenterRating { get; set; }
+    }
+}
---
+++ Boats-4-U.WebAPI/Controllers/RenterRatingController.cs
@@ -0,0 +1,45 @@
+﻿using Boats_4_U.Models;
+using Boats_4_U.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Boats_4_U.WebAPI.Controllers
+{
+    public class RenterRatingController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(RenterRatingCreate rating)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRenterRatingService();
+
+            if (!service.CreateRenterRating(rating))
+                return InternalServerError();
+
+            return Ok("Rating was succesffully created");
+        }
+
+        [HttpGet]
+        [Route("api/RenterRating/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            var service = CreateRenterRatingService();
+            var rating = service.GetRenterRatingById(id);
+            return Ok(rating);
+        }
+
+        private RenterRatingService CreateRenterRatingService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var ratingService = new RenterRatingService(userId);
+            return ratingService;
+        }
+    }
+}
