6dd7d4ae24b0d9e27e7b2a685e6865db94cfe589
adamkr13 <adamreuter13@gmail.com>
2021-03-12 09:37:43 -0500

    update

---
+++ Boats-4-U.Data/Migrations/202103111836103_UserNameFix.cs
@@ -0,0 +1,24 @@
+﻿namespace Boats_4_U.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserNameFix : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DriverRating", "UserCreatedDriverRating", c => c.String());
+            AddColumn("dbo.Driver", "UserCreatedDriver", c => c.String());
+            AddColumn("dbo.Renter", "UserCreatedRenter", c => c.String());
+            AddColumn("dbo.RenterRating", "UserCreatedRenterRating", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.RenterRating", "UserCreatedRenterRating");
+            DropColumn("dbo.Renter", "UserCreatedRenter");
+            DropColumn("dbo.Driver", "UserCreatedDriver");
+            DropColumn("dbo.DriverRating", "UserCreatedDriverRating");
+        }
+    }
+}
